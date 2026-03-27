b895d8e5f896b8c5720137d58c3dec980ffb39af
Nethu <nilakshinethmi48@gmail.com>
2026-03-27 22:24:22 +0530

    add SSL and public key retrieval option

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     apps : [
         {
+            name: "cloud-sql-auth-proxy",
+            script: "./cloud-sql-proxy eca-project-491209:asia-southeast1:mysql-vm eca-project-491209:asia-southeast1:postgres-vm --private-ip",
+            log_file: "./logs/cloud-sql-auth-proxy.log"
+        },
+
+        {
       name   : "student-service",
       script : "java -jar student-service/target/Student-Service-1.0.0.jar",
             log_file: "./logs/student-service.log",
